f6af728628baf0c34171ad72317786232aee4d2c
Jackson <jackson.gleeson@amtek.com.au>
2025-05-19 16:58:19 +1000

    index.html

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TicketingSystem
 This is a simple Python + FastAPI project to create and manage IT tickets
 
-Run with `uvicorn app.main:app --port 9090 --reload`
+Run with 
+`uvicorn app.main:app --port 9090 --reload`
--- app/ticket_routes.py
+++ app/ticket_routes.py
@@ -13,16 +13,19 @@ async def home(request: Request):
     tickets = res.fetchall()
     return templates.TemplateResponse("index.html", {"request": request, "tickets": tickets})
 
-@router.get("/tickets")
+@router.get("/tickets", response_class=HTMLResponse)
 async def tickets(request: Request):
-    return templates.TemplateResponse("create-ticket.html", {"request": request})
+    tickets = cur.execute("SELECT * FROM tickets").fetchall()
+    return templates.TemplateResponse("tickets.html", {"request": request, "tickets": tickets})
 
-@router.post("/createticket")
+@router.post("/createticket", response_class=HTMLResponse)
 async def create_ticket(ticket_title: str = Form(...), ticket_body: str = Form(...), priority: str = Form(...)):
     res = cur.execute(f"INSERT INTO tickets (ticket_title, ticket_body, priority) VALUES (?, ?, ?)", (ticket_title, ticket_body, priority))
-    con.commit()
-    ticket_id = cur.lastrowid
-    return templates.TemplateResponse("create-ticket.html", {"request": {}, "ticket": (ticket_id, ticket_body, priority)})
+    return HTMLResponse("<p>Ticket Created</p>")
+
+@router.get("/ticket-form", response_class=HTMLResponse)
+async def ticket_form(request: Request):
+    return templates.TemplateResponse("create-ticket.html", {"request": request})
 
 @router.get("/getticket")
 async def get_ticket(ticket_id: int):
